7a8ccfa977bcdc4938d171e8236882f6378ec1b6
linfeiyang <329379172@qq.com>
2016-05-03 20:56:20 +0800

    first commit

---
+++ action/survey.js
@@ -0,0 +1,25 @@
+/**
+ * Created by linfeiyang on 5/3/16.
+ */
+var model = require('../lib/model');
+
+
+
+exports.commit = function(req, res, next){
+    //console.log(req.body);
+    var body = req.body || {};
+    body.ip = getClientAddress(req);
+    model.smartyAdd('record', body, true, function(err, result){
+        if(err) {
+            console.log(err);
+            return res.json({bianhao: 0, status: "失败"});
+        }
+        console.log(result);
+        return res.json({bianhao: 1, status: "成功"});
+    });
+};
+
+getClientAddress = function (req) {
+    return (req.headers['x-forwarded-for'] || '').split(',')[0]
+        || req.connection.remoteAddress;
+};
---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by linfeiyang on 5/3/16.
+ */
+var config = {
+    mysql: {
+        host     : 'mysql',
+        user     : 'root',
+        password : '123456',
+        database : 'doudou'
+    }
+};
+
+module.exports = config;
